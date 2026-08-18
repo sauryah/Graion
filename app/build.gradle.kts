893fafc0b88a04113249a295d76efc216f71b4b4
sahil <sauryah@protonmail.com>
2026-08-19 00:00:39 +0530

    build: bump versionCode to 2 (versionName 1.0.1) for Google Play release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sauryah.lihas.calculator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     val keystorePropertiesFile = rootProject.file("keystore.properties")
